a55ac035866ac5ec1921289759ac8672150330f1
Randy Grullon <54919808+RandyGrullon@users.noreply.github.com>
2021-09-17 23:45:03 -0400

    codigo

--- test/main.test.js
+++ test/main.test.js
@@ -1,10 +1,10 @@
 const { expect } = require("@jest/globals");
-const {ValidateAnISBN} = require("../main.js")
+const ValidateAnISBN = require("../main.js")
 
-test('ISBN of 10 Digit', () => {
+test('ISBN of 10 Digit'), () => {
     expect(ValidateAnISBN('817525766-0')).toBe('ISBN Valido para 10 digitos')
-})
+}
 
-test('ISBN of 13 Digit', () => {
+test('ISBN of 13 Digit'), () => {
     expect(ValidateAnISBN('978-987-988-0909')).toBe('ISBN of 13 Digit')
-})
+}
